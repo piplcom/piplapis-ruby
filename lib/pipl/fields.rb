ea3f40a7f930747103fae4c0433e1420dfbd0479
willemorvets <willem.orvets@teamnorthwoods.com>
2024-01-17 14:21:26 -0500

    calling super on to_hash for field so I can get the attr_accessor properties on the base class

--- lib/pipl/fields.rb
+++ lib/pipl/fields.rb
@@ -42,8 +42,8 @@ module Pipl
       []
     end
 
-    def to_hash
-
+    def to_hash(options)
+      options.merge(valid_since: @valid_since, last_seen: @last_seen, inferred: @inferred, current: @current).reject { |_, value| value.nil? }
     end
 
     def is_searchable?
@@ -412,7 +412,7 @@ module Pipl
     end
 
     def to_hash
-      {url: @url, thumbnail_token: @thumbnail_token }.reject { |_, value| value.nil? }
+      super({url: @url, thumbnail_token: @thumbnail_token })
     end
 
   end
